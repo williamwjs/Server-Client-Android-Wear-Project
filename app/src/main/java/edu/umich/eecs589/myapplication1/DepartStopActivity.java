d5fb3b8f1b4c8debd87ed61c72d6ce3d2507abd6
williamwjs <williamwjs.sjtu@gmail.com>
2015-12-11 23:57:13 -0500

    Half GCM

--- app/src/main/java/edu/umich/eecs589/myapplication1/DepartStopActivity.java
+++ app/src/main/java/edu/umich/eecs589/myapplication1/DepartStopActivity.java
@@ -1,13 +1,23 @@
 package edu.umich.eecs589.myapplication1;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.speech.RecognizerIntent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.List;
 
@@ -15,8 +25,10 @@ public class DepartStopActivity extends Activity {
 
     private TextView mTextView;
     private String departStop;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     private final static int SPEECH_REQUEST_CODE = 0;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private final static String TAG = "CURRENT";
 
     @Override
@@ -30,7 +42,35 @@ public class DepartStopActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.departStop);
             }
         });
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+            }
+        };
         departStop = "none";
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 
     public void saveDepartStop(View view) {
@@ -61,5 +101,26 @@ public class DepartStopActivity extends Activity {
             mTextView.setText(departStop);
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/edu/umich/eecs589/myapplication1/QuickstartPreferences.java
@@ -0,0 +1,11 @@
+package edu.umich.eecs589.myapplication1;
+
+/**
+ * Created by willwjs on 12/11/15.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
